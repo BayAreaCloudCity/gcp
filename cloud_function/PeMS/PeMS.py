cb9a6547ceb505c88529e3b096416dd0da97dd6b
Qingyang Hu <huqingyang2000@gmail.com>
2024-01-21 00:51:32 +0000

    Use env variables

--- cloud_function/PeMS/PeMS.py
+++ cloud_function/PeMS/PeMS.py
@@ -1,10 +1,13 @@
 import gzip
 import os
+from io import StringIO
+from typing import List
 from zoneinfo import ZoneInfo
+
+import functions_framework
 import requests
 import csv
 from datetime import datetime, timedelta
-import json
 from cloudevents.http import CloudEvent
 from google.cloud import bigquery
 from google.protobuf import json_format
@@ -12,6 +15,7 @@ from pems_pb2 import PeMS
 import tqdm
 
 BASE_URL = "https://pems.dot.ca.gov"
+DISTRICT_ID = 4
 
 '''
 Environment Variables:
@@ -23,9 +27,18 @@ TABLE_ID: Table ID to check for existing records
 '''
 
 
-def get_pems_data(days_to_fetch=None):
-    if days_to_fetch is None:
+@functions_framework.cloud_event
+def collect_pems_data(cloud_event: CloudEvent):
+    download()
+
+
+def download(days_to_fetch: List = None):
+    if days_to_fetch is None or len(days_to_fetch) == 0:
         days_to_fetch = [(datetime.now(ZoneInfo("America/Los_Angeles")) - timedelta(days=1)).strftime('%Y_%m_%d')]
+
+    print(f"Download {days_to_fetch} from PeMS.")
+    if len(list(filter(lambda x: not x.startswith(days_to_fetch[0][:4]), days_to_fetch))) != 0:
+        raise Exception("days_to_fetch does not stay within the same year")
 
     session = requests.Session()
     # Log in
@@ -39,8 +52,8 @@ def get_pems_data(days_to_fetch=None):
     )
 
     months = session.get(BASE_URL, params={"srq": "clearinghouse",
-                                           "district_id": 4,
-                                           "yy": 2024,
+                                           "district_id": DISTRICT_ID,
+                                           "yy": days_to_fetch[0][:4],
                                            "type": "station_5min",
                                            "returnformat": "text"})
     months.raise_for_status()
@@ -48,10 +61,10 @@ def get_pems_data(days_to_fetch=None):
     for files in months.json()['data'].values():
         for file in files:
             if any(day in file['file_name'] for day in days_to_fetch):
+                print(f"Upload {file['file_name']}.")
                 gzipped_data = session.get(BASE_URL + file['url'])
                 gzipped_data.raise_for_status()
-                data = gzip.decompress(gzipped_data.content)
-                print(data)
+                upload(gzip.decompress(gzipped_data.content))
 
 
 def parse_int(val: str):
@@ -62,113 +75,44 @@ def parse_float(val: str):
     return float(val) if len(val) > 0 else None
 
 
-def upload_to_bigquery():
-    client = bigquery.Client(project="cloud-city-cal")
-    job_config = bigquery.LoadJobConfig(schema=client.get_table("cloud_city.pems").schema)
+def upload(pems_data: bytes):
+    client = bigquery.Client(project=os.environ['PROJECT_ID'])
+    job_config = bigquery.LoadJobConfig(schema=client.get_table(f"{os.environ['DATASET_ID']}.{os.environ['TABLE_ID']}").schema)
     data = []
 
-    with gzip.open("sample.txt.gz", "rt") as f:
-        reader = csv.reader(f, delimiter=",")
-        for row in tqdm.tqdm(reader):
-            pems = PeMS(
-                time=row[0],
-                station_id=parse_int(row[1]),
-                district=parse_int(row[2]),
-                freeway=parse_int(row[3]),
-                direction=row[4],
-                lane_type=row[5],
-                station_length=parse_float(row[6]) if len(row[6]) > 0 else 0.0,
-                samples=parse_int(row[7]),
-                percentage_observed=parse_int(row[8]),
-                total_flow=parse_int(row[9]),
-                average_occupancy=parse_float(row[10]),
-                average_speed=parse_float(row[11]),
-                lanes=[]
-            )
-            offset = 12
-            for i in range(0, int((len(row) - offset) / 5)):
-                if row[offset + i * 5 + 0] == '' and parse_int(row[offset + i * 5 + 4]) == 0:
-                    continue # empty record
+    reader = csv.reader(StringIO(pems_data.decode()), delimiter=",")
+    for row in tqdm.tqdm(reader):
+        pems = PeMS(
+            time=row[0],
+            station_id=parse_int(row[1]),
+            district=parse_int(row[2]),
+            freeway=parse_int(row[3]),
+            direction=row[4],
+            lane_type=row[5],
+            station_length=parse_float(row[6]) if len(row[6]) > 0 else 0.0,
+            samples=parse_int(row[7]),
+            percentage_observed=parse_int(row[8]),
+            total_flow=parse_int(row[9]),
+            average_occupancy=parse_float(row[10]),
+            average_speed=parse_float(row[11]),
+            lanes=[]
+        )
+        offset = 12
+        for i in range(0, int((len(row) - offset) / 5)):
+            if row[offset + i * 5 + 0] == '' and parse_int(row[offset + i * 5 + 4]) == 0:
+                continue  # empty record
 
-                pems.lanes.append(
-                    PeMS.Lane(
-                        samples=parse_int(row[offset + i * 5 + 0]),
-                        flow=parse_int(row[offset + i * 5 + 1]),
-                        average_occupancy=parse_float(row[offset + i * 5 + 2]),
-                        average_speed=parse_float(row[offset + i * 5 + 3]),
-                        observed=parse_int(row[offset + i * 5 + 4]) == 1
-                    ))
+            pems.lanes.append(
+                PeMS.Lane(
+                    samples=parse_int(row[offset + i * 5 + 0]),
+                    flow=parse_int(row[offset + i * 5 + 1]),
+                    average_occupancy=parse_float(row[offset + i * 5 + 2]),
+                    average_speed=parse_float(row[offset + i * 5 + 3]),
+                    observed=parse_int(row[offset + i * 5 + 4]) == 1
+                ))
 
-            data.append(json_format.MessageToDict(pems, preserving_proto_field_name=True))
+        data.append(json_format.MessageToDict(pems, preserving_proto_field_name=True))
 
     client.load_table_from_json(
         data,
-        "cloud_city.pems", job_config=job_config).result()
-    exit()
-
-
-#     publisher_client = PublisherClient()
-#     topic_path = publisher_client.topic_path(os.environ['PROJECT_ID'], os.environ['TOPIC_ID'])
-#
-#     # Crawl Data
-#     for station_id in stations:
-#         print(f"Fetching data for station_id: {station_id}.")
-#         params = {
-#             "report_form": 1,
-#             "dnode": "VDS",
-#             "content": "loops",
-#             "export": "text",
-#             "station_id": station_id,
-#             "s_time_id": timestamp_now - TIME_RANGE,
-#             "e_time_id": timestamp_now,
-#             "dow_0": "on",
-#             "dow_1": "on",
-#             "dow_2": "on",
-#             "dow_3": "on",
-#             "dow_4": "on",
-#             "dow_5": "on",
-#             "dow_6": "on",
-#             "dow_7": "on",
-#             "holidays": "on",
-#             "q": "speed",
-#             "q2": "flow",
-#             "gn": "5min",
-#             "agg": "on",
-#             "lane1": "on",
-#             "lane2": "on",
-#             "lane3": "on",
-#             "lane4": "on",
-#             "lane5": "on",
-#             "lane6": "on",
-#             "lane7": "on",
-#         }
-#         res = session.post(
-#             BASE_URL,
-#             params=params,
-#         )
-#         res.raise_for_status()
-#         reader = csv.DictReader(StringIO(res.text), delimiter="\t", quoting=csv.QUOTE_NONE)
-#         for row in reader:
-#             lanes_len = int(row["# Lane Points"])
-#             data = PeMS(station_id=station_id, time=row["5 Minutes"], observed=float(row["% Observed"]), lanes=[])
-#             for i in range(1, lanes_len + 1):
-#                 data.lanes.append(PeMS.Lane(speed=float(row[f"Lane {i} Speed (mph)"]),
-#                                             flow=float(row[f"Lane {i} Flow (Veh/5 Minutes)"])))
-#             publisher_client.publish(topic_path, data.SerializeToString())
-#
-#
-# def get_missing_record_ids() -> List:
-#     client = bigquery.Client(project=os.environ['PROJECT_ID'])
-#
-#     query_job = client.query(
-#         f"""
-#         SELECT station_id, MAX(`time`) as lastest_time
-#         FROM {os.environ['PROJECT_ID']}.{os.environ['DATASET_ID']}.{os.environ['TABLE_ID']}
-#         GROUP BY station_id
-#         """)
-#
-#     return query_job.result()
-
-
-if __name__ == '__main__':
-    upload_to_bigquery()
+        f"{os.environ['DATASET_ID']}.{os.environ['TABLE_ID']}", job_config=job_config).result()
